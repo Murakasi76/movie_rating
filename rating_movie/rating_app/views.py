67f1a2397d0a01b519ce65d1e504fd7d9491700c
Ruslan Bihun <bigun.ruslan@gmail.com>
2025-09-03 09:33:22 +0200

    add urls and path for rating_app.views

--- rating_movie/rating_app/views.py
+++ rating_movie/rating_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+
+def index(request: HttpRequest):
+    if request.method == "POST":
+        pass
+    elif request.method == "GET":
+        pass
+    
+    context = {
+        
+    }
+    return HttpResponse("Hello World")
