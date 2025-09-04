15b8f2a972c81b935ba34c323d8e7245efd0e0c0
Ruslan Bihun <bigun.ruslan@gmail.com>
2025-09-04 06:00:51 +0200

    add migrate with DB 0001

--- rating_movie/rating_app/models.py
+++ rating_movie/rating_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=150)
+    genre = models.CharField(max_length=50)
+    release_date = models.DateField()
+    rating = models.FloatField(null=True, blank=True)
+    
+    class Meta:
+        db_table = "movie"
+        
+    def __str__(self):
+        return self.title
+    
