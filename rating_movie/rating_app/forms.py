f85dea319a310f46b7953d9f75c581c0cbfe78c7
Ruslan Bihun <bigun.ruslan@gmail.com>
2025-09-04 07:49:39 +0200

    add widgets in class Meta in class novieForm

--- rating_movie/rating_app/forms.py
+++ rating_movie/rating_app/forms.py
@@ -6,3 +6,8 @@ class MovieForm(forms.ModelForm):
     class Meta:
         model = Movie
         fields = ["title", "genre", "release_date", "rating"]
+        widgets = {
+            "release_date": forms.DateInput(format="%Y-%m-%d", attrs={"type": "date"}),
+            "title": forms.TextInput(attrs={"class": "content_title", "placeholder": "Enter book title "}),
+            
+        }
