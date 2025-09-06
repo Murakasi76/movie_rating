f44a71d1d4a7fb2d8be5cfc0a74776d671ed50e0
Ruslan Bihun <bigun.ruslan@gmail.com>
2025-09-06 09:52:56 +0200

    custom admin.py

--- rating_movie/rating_app/admin.py
+++ rating_movie/rating_app/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from rating_app.models import Movie
 
 # Register your models here.
+from django.contrib.auth.models import User
 
 admin.site.register(Movie)
+# admin.site.unregister(User)
