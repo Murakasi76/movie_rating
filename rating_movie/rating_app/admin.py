1d85966733db217fcc8764e48277456f46452c91
Ruslan Bihun <bigun.ruslan@gmail.com>
2025-09-08 07:49:54 +0200

    add filter ordering and search in admin.py

--- rating_movie/rating_app/admin.py
+++ rating_movie/rating_app/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from rating_app.models import Movie
+from rating_app.forms import MovieForm
 
 # Register your models here.
 from django.contrib.auth.models import User
 
-admin.site.register(Movie)
+# admin.site.register(Movie)
 # admin.site.unregister(User)
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    form = MovieForm
+    # list_display = ("title", "genre", "release_date", "rating",)
+    list_filter = ("genre", "release_date",)
+    search_fields = ("title__startswith",)
+    ordering = ("-release_date",)
